88facb9ace740a5aea6e402a6ce25d060a1cb5b1
Steven Longmire <stevenlongmire@ufl.edu>
2021-07-06 16:30:46 -0400

    Add files via upload
    initial upload

---
+++ R/script/redfish.R
@@ -0,0 +1,18 @@
+#
+#Start date 7/6/2021
+#
+#This is the r script for exploratory analysis of the output FACT data
+
+install.packages('tidyverse')
+install.packages('lubridate')
+install.packages('plotly')
+install.packages('ggplot2')
+install.packages('remotes')
+
+library(tidyverse)
+library(lubridate)
+library(plotly)
+library(ggplot2)
+library(remotes)
+
+
